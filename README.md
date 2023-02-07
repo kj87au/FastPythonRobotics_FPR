b57a042992e4a86cfda57cfe0351efc4cde069f9
Keith Hall <keithjhall87@gmail.com>
2023-02-07 12:44:33 +1000

    inital commit, sorting out dependencies and adding skeloton folders.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # FastPythonRobotics_FPR
-A Python robotics library optimised.
+A Python robotics library optimised using Numbas LLVM compiler. Borrows heavily from https://github.com/AtsushiSakai/PythonRobotics but is faster and more modular.
---
+++ scripts/localisation/run_particle_filter.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+"""
+Author: Keith Hall (keithjhall87@gmail.com)
+Created on: Tuesday Feb 07, 2023 12:32:48 AEST
+File name: run_particle_filter
+Licence: MIT
+Description: 
+"""
+
+# IMPORTS
+import matplotlib.pyplot as plt
+import numpy as np
+
+# GLOBALS
+# Estimation parameter of PF
+Q = np.diag([0.2]) ** 2  # range error
+R = np.diag([2.0, np.deg2rad(40.0)]) ** 2  # input error
+
+#  Simulation parameter
+Q_sim = np.diag([0.2]) ** 2
+R_sim = np.diag([1.0, np.deg2rad(30.0)]) ** 2
+
+DT = 0.1  # time tick [s]
+SIM_TIME = 50.0  # simulation time [s]
+MAX_RANGE = 20.0  # maximum observation range
+
+# Particle filter parameter
+NP = 100  # Number of Particle
+NTh = NP / 2.0  # Number of particle for re-sampling
+
+show_animation = True
+
+
+def main():
+    def main():
+    print(__file__ + " start!!")
+
+    time = 0.0
+
+    # RF_ID positions [x, y]
+    rf_id = np.array([[10.0, 0.0],
+                      [10.0, 10.0],
+                      [0.0, 15.0],
+                      [-5.0, 20.0]])
+
+    # State Vector [x y yaw v]'
+    x_est = np.zeros((4, 1))
+    x_true = np.zeros((4, 1))
+
+    px = np.zeros((4, NP))  # Particle store
+    pw = np.zeros((1, NP)) + 1.0 / NP  # Particle weight
+    x_dr = np.zeros((4, 1))  # Dead reckoning
+
+    # history
+    h_x_est = x_est
+    h_x_true = x_true
+    h_x_dr = x_true
+
+    while SIM_TIME >= time:
+        time += DT
+        u = calc_input()
+
+        x_true, z, x_dr, ud = observation(x_true, x_dr, u, rf_id)
+
+        x_est, PEst, px, pw = pf_localization(px, pw, z, ud)
+
+        # store data history
+        h_x_est = np.hstack((h_x_est, x_est))
+        h_x_dr = np.hstack((h_x_dr, x_dr))
+        h_x_true = np.hstack((h_x_true, x_true))
+
+        if show_animation:
+            plt.cla()
+            # for stopping simulation with the esc key.
+            plt.gcf().canvas.mpl_connect(
+                'key_release_event',
+                lambda event: [exit(0) if event.key == 'escape' else None])
+
+            for i in range(len(z[:, 0])):
+                plt.plot([x_true[0, 0], z[i, 1]], [x_true[1, 0], z[i, 2]], "-k")
+            plt.plot(rf_id[:, 0], rf_id[:, 1], "*k")
+            plt.plot(px[0, :], px[1, :], ".r")
+            plt.plot(np.array(h_x_true[0, :]).flatten(),
+                     np.array(h_x_true[1, :]).flatten(), "-b")
+            plt.plot(np.array(h_x_dr[0, :]).flatten(),
+                     np.array(h_x_dr[1, :]).flatten(), "-k")
+            plt.plot(np.array(h_x_est[0, :]).flatten(),
+                     np.array(h_x_est[1, :]).flatten(), "-r")
+            plot_covariance_ellipse(x_est, PEst)
+            plt.axis("equal")
+            plt.grid(True)
+            plt.pause(0.001)
+
+
+if __name__ == "__main__":
+    main()
