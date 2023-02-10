87f416e174d0d4d1d1f0d45efde7dd3a10f696dc
Keith Hall <keithjhall87@gmail.com>
2023-02-10 12:37:44 +1000

    continuing with project file setup

---
+++ tests/integration/test_integration_filters.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Author: Keith Hall (keithjhall87@gmail.com)
+Created on: Friday Feb 10, 2023 12:36:26 AEST
+File name: test_intergration_filters
+Licence: MIT
+Description: 
+"""
+
+# IMPORTS
+
+
+# GLOBALS
+
+
+def test_integration_filters():
+    assert 1 == 1
---
+++ tests/unit/test_filters.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Author: Keith Hall (keithjhall87@gmail.com)
+Created on: Friday Feb 10, 2023 12:25:06 AEST
+File name: test_filters
+Licence: MIT
+Description: Unit tests for the Filters of FPR 
+"""
+
+# IMPORTS
+from FPR.Filters.ParticleFilter import calc_input
+
+# GLOBALS
+
+
+
+def test_pytest():
+    assert 1 == 1
